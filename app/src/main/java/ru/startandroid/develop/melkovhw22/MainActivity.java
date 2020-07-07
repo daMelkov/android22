eaa663bed3f431ecde2944d1de57cc334215a55e
Melkov Denis <d.a.melkov@gmail.com>
2020-07-07 17:07:05 +0300

    add reaction to buttons

--- app/src/main/java/ru/startandroid/develop/melkovhw22/MainActivity.java
+++ app/src/main/java/ru/startandroid/develop/melkovhw22/MainActivity.java
@@ -2,15 +2,44 @@ package ru.startandroid.develop.melkovhw22;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    final private static String URL = "http://myfile.org/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /* Кнопка "Назад" */
+        Button btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
+        /* Кнопка "Вперёд" */
+        Button btnForward = findViewById(R.id.btnForward);
+        btnForward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        /* URL фотографии */
+        TextView textView = findViewById(R.id.url);
+        textView.setText(URL + new Random().nextInt(100));
     }
 }
